2ee29ffcbf8f227f5b729429774bbbb6c9f8966c
Serge Garbay <serge.garbay@education.lu>
2025-06-02 21:35:59 +0200

    New version

--- orika/student_000/params.js
+++ orika/student_000/params.js
@@ -1,27 +1,18 @@
-// CHANGE ONLY THIS FILE!
-const params = {
-  bgColor: '#f0f8ff', // Light blue background
-  textColor: '#333333', // Dark text for readability
-  headerBg: '#ff69b4', // Bright pink header
-  borderColor: '#ffa500', // Orange borders
-  headerText: 'Welcome to My Awesome Self!',
-  buttonText: 'Drück Mich!',
-  buttonMessage: 'Haha ugeschmiert!',
-  buttonBg: '#32cd32', // Lime green button
-  buttonTextColor: '#ffffff',
+// Kids only change values below
 
-  // Main Pokémon image
-  mainImage: '../images/pokemon/001_Bulbasaur.gif',
+const settings = {
+  kidName: "Alex", 
+  headerText: "Welcome to my website!",
+  bgColor: "#e6f7ff",
+  textColor: "#333333",
+  headerBg: "#ffb347",
+  borderColor: "#ff69b4",
 
-  // Favorite food
-  favoriteFoodImage: '../images/food/pizza.jpg',
-  favoriteFoodText: 'Pizza with lots of cheese and pepperoni!',
+  // Pokémon partner
+  pokemonImage: "../images/pokemon/025_pikachu.gif",
 
-  // Favorite animal
-  favoriteGameImage: '../images/animals/dog1.jpg',
-  favoriteGameText: 'I love dogs because they are loyal and fun!',
-
-  // Future job wish
-  funFactImage: '../images/food/pizza.jpg',
-  funFactText: 'I want to be a scientist and discover cool things!'
+  // Trivia answers
+  foodImage: "../images/food/pizza.jpg",
+  animalImage: "../images/animals/dog1.jpg",
+  jobImage: "../images/jobs/astronaut.jpg"
 };
